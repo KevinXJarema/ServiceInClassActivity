14aad7ec87d32a8ded8fd0368aca85bc7b7c3618
KevinXJarema <tun48388@temple.edu>
2023-11-28 15:20:25 -0500

    Created a menu to perform the task of the timer service rather than buttons

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -8,6 +8,8 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.IBinder
 import android.os.Looper
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.TextView
 import java.util.Timer
@@ -35,11 +37,31 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        menuInflater.inflate(R.menu.main, menu)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean{
+
+        when(item.itemId){
+
+            R.id.action_start -> if(isConnected) timerBinder.start(10)
+            R.id.action_pause -> if(isConnected) timerBinder.pause()
+            R.id.action_stop -> if(isConnected) timerBinder.stop()
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         timerTextView = findViewById(R.id.timerTextView)
+
+
+
 
         bindService(
             Intent(this, TimerService::class.java),
@@ -48,18 +70,6 @@ class MainActivity : AppCompatActivity() {
 
         )
 
-
-        findViewById<Button>(R.id.startButton).setOnClickListener {
-            if(isConnected) timerBinder.start(10)
-        }
-
-        findViewById<Button>(R.id.pauseButton).setOnClickListener {
-            if(isConnected) timerBinder.pause()
-        }
-        
-        findViewById<Button>(R.id.stopButton).setOnClickListener {
-            if(isConnected) timerBinder.stop()
-        }
     }
 
     override fun onDestroy() {
